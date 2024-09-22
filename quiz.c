af80f439f0d312afd046c6aed23042a757cbb814
RushilKarani <karanir@mcmaster.ca>
2024-09-21 23:51:45 -0400

    making the CI pipeline assignment repo

---
+++ quiz.c
@@ -0,0 +1,14 @@
+/* CI Pipeline Assignment
+*
+* Rushil Karani
+* September 11th, 2024
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    puts("Hello, World!");
+    return EXIT_SUCCESS;
+}
+
